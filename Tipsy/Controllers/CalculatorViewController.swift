0be1238be096a7ad553f06744fb19eef4326d94d
zcredi <zcredi@gmail.com>
2022-11-24 18:25:05 +0300

    Calculate the Bill

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var splitNumber = 2
+    var billTotal = 0.0
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        if sender.isSelected {
+                return
+            }
+
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        tip = buttonTitleAsANumber / 100
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        splitNumber = Int(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let bill = billTextField.text!
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(splitNumber)
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+            print(resultTo2DecimalPlaces)
+        }
+    }
+    
+    @IBAction func billText(_ sender: UITextField) {
+
+    }
+}
---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Владислав on 23.11.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
